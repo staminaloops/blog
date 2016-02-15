20608f920945a5ec40c2503bea97c70d5d24c55c
staminaloops <staminaloops@gmail.com>
2016-02-15 14:51:26 +0000

    gulp integration

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var shell = require('gulp-shell');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+var cssPath = 'themes/material-design/static/css/*.css';
+var jsPath = 'themes/material-design/static/js/*.js'
+
+gulp.task('css', function() {
+  return gulp.src(cssPath)
+    .pipe(autoprefixer())
+    .pipe(concat('all.min.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('themes/material-design/static/dist/css'))
+});
+
+gulp.task('js', function() {
+  return gulp.src(jsPath)
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('themes/material-design/static/dist/js'))
+});
+
+gulp.task('images', function() {
+  return gulp.src('static/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('static/images'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(cssPath, ['css']);
+  gulp.watch(jsPath, ['js']);
+});
+
+gulp.task('serve', ['css', 'js', 'watch'], shell.task([
+  'hugo server -t material-design -w'
+]));
+
+gulp.task('build', ['css', 'js', 'images'], shell.task([
+  'hugo -t material-design'
+]));
